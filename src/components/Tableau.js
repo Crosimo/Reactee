d26c803a15b0a8517f6d88751becb56be9e67779
Nasila <fushs@hotmail.fr>
2021-07-07 10:11:10 +0200

    ez

--- src/components/Tableau.js
+++ src/components/Tableau.js
@@ -1,5 +1,7 @@
 import React, { Component } from 'react';
 import { Doughnut } from 'react-chartjs-2'
+
+
 class Tableau extends Component {
     render() {
         let set1 = new Set(JSON.parse(localStorage.getItem('grapheJour')));
